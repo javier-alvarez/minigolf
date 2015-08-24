260bd13f3f658340f012fd4342bc246d700083b4
Jo Osborne <joanne.osborne@gmail.com>
2015-08-24 11:53:54 +0100

    Remove copied SmallSharpLibrary files

--- SmallSharpLibrary/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace Library
-
-open System
-open System.Threading
-
-[<Sealed>]
-type Program private () =
-   static member Delay(ms:int) = Thread.Sleep(ms)
-   static member End() = Environment.Exit(0)     
